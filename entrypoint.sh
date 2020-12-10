3dc588775f36e7baacd975fbd22533be0763c7b4
Adam J. Volin <avolin@Jupiter.local>
2020-12-10 12:50:39 -0500

    Moved migrate command in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -f /channel_mapper/database.sqlite ] || { touch /channel_mapper/database.sqlite; chmod -R 777 /channel_mapper; php artisan migrate; }
+[ -f /channel_mapper/database.sqlite ] || { touch /channel_mapper/database.sqlite; chmod -R 777 /channel_mapper; }
 [ -e /channel_mapper/storage ] || { mv /usr/src/app/storage /channel_mapper/; ln -s /channel_mapper/storage /usr/src/app/storage; }
 
 cd /usr/src/app
@@ -8,10 +8,11 @@ cd /usr/src/app
 sed '/^CHANNELS_SERVER_IP=/{h;s/=.*/='"$CHANNELS_SERVER_IP"'/};${x;/^$/{s//CHANNELS_SERVER_IP='"$CHANNELS_SERVER_IP"'/;H};x}' -i .env
 sed '/^CHANNELS_SERVER_PORT=/{h;s/=.*/='"$CHANNELS_SERVER_PORT"'/};${x;/^$/{s//CHANNELS_SERVER_PORT='"$CHANNELS_SERVER_PORT"'/;H};x}' -i .env
 
+php artisan migrate;
+
 while :
 do
     echo "Channels server: $CHANNELS_SERVER_IP:$CHANNELS_SERVER_PORT"
     echo "Starting application"
     php artisan serve --host=0.0.0.0 --port=80
 done
-
