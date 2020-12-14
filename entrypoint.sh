21c94ea9425bacd7126bca44318f998a9bd093ea
Adam J. Volin <avolin@Jupiter.local>
2020-12-14 14:46:23 -0500

    entrypoint updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-[ -f /usr/src/app/.env ] || { cp /usr/src/app/.env.channels /usr/src/app/.env; php artisan key:generate; }
-[ -f /channel_mapper/database.sqlite ] || { touch /channel_mapper/database.sqlite; chmod -R 777 /channel_mapper; ln -s /channel_mapper/database.sqlite /usr/src/app/database/database.sqlite; }
-[ -e /channel_mapper/storage ] || { mv /usr/src/app/storage /channel_mapper/; ln -s /channel_mapper/storage /usr/src/app/storage; }
+# Ensure database file exists
+touch /channel_mapper/database.sqlite
+# Symlink database to app database folder
+ln -s /channel_mapper/database.sqlite /usr/src/app/database/database.sqlite
 
+# Move app storage folder to bound volume if it doesn't exist
+[ -e /channel_mapper/storage ] || { mv /usr/src/app/storage /channel_mapper/; }
+# Symlink bound storage folder to app storage folder
+ln -s /channel_mapper/storage /usr/src/app/storage
+
+# Initialize .env file if it doesn't exist
+[ -f /channel_mapper/.env ] || { cp /usr/src/app/.env.channels /channel_mapper/.env; ln -s /channel_mapper/.env /usr/src/app/.env; chmod 777 /channel_mapper/.env; php artisan key:generate; }
+
+# Set permissions on bound volume
+chmod -R 777 /channel_mapper
+
+# Change working directory to app folder
 cd /usr/src/app
 
+# Set .env vars for Channels server IP(s)
 sed '/^CHANNELS_SERVER_IP=/{h;s/=.*/='"$CHANNELS_SERVER_IP"'/};${x;/^$/{s//CHANNELS_SERVER_IP='"$CHANNELS_SERVER_IP"'/;H};x}' -i .env
 sed '/^CHANNELS_SERVER_PORT=/{h;s/=.*/='"$CHANNELS_SERVER_PORT"'/};${x;/^$/{s//CHANNELS_SERVER_PORT='"$CHANNELS_SERVER_PORT"'/;H};x}' -i .env
 
@@ -21,6 +35,7 @@ else
     sed '/^CHANNELS_SERVER_PORT_FOR_PLAYLIST=/{h;s/=.*/='"$CHANNELS_SERVER_PORT_FOR_PLAYLIST"'/};${x;/^$/{s//CHANNELS_SERVER_PORT_FOR_PLAYLIST='"$CHANNELS_SERVER_PORT_FOR_PLAYLIST"'/;H};x}' -i .env
 fi
 
+# Run database migrations
 php artisan migrate --force
 
 while :
